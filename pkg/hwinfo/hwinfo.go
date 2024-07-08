9419c306e967e0716ccbd5983742396db5eb65b5
Brian McGee <brian@bmcgee.ie>
2024-07-08 10:37:00 +0100

    feat: remove debug and log from report
    They might be useful for debugging at some point,
    but we shouldn't include them in the report.
    
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- pkg/hwinfo/hwinfo.go
+++ pkg/hwinfo/hwinfo.go
@@ -18,8 +18,6 @@ func Scan() (*Report, error) {
 	defer C.hd_free_hd_data(data)
 
 	report := Report{}
-	report.Log = C.GoString(data.log)
-	report.Debug = uint(data.debug)
 
 	for hd := data.hd; hd != nil; hd = hd.next {
 		item, err := NewItem(hd)
