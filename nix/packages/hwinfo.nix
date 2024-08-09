77c54df3604f997a8e6d344bc4bd0568e0f72eea
Brian McGee <brian@bmcgee.ie>
2024-08-09 19:04:42 +0100

    feat: use a fork of hwinfo
    Upstream, the pci and usb ids were last imported in 2021.
    
    Switching to this fork is a short-term fix until its changes have been merged upstream https://github.com/openSUSE/hwinfo/pull/144.
    
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- nix/packages/hwinfo.nix
+++ nix/packages/hwinfo.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}:
 # we use this fork for updated pci and usb id resolution until https://github.com/openSUSE/hwinfo/pull/144 is
 # merged upstream
-pkgs.hwinfo.overrideAttrs (_old: {
+pkgs.hwinfo.overrideAttrs {
   src = pkgs.fetchFromGitHub {
     owner = "brianmcgee";
     repo = "hwinfo";
     rev = "6fa35c4e8404217a6dc57cff1f5639f7b596be18";
     hash = "sha256-HYLoTArnwZsIVzNFtPeJus9inDKx3kYJSKWIu5IPp14=";
   };
-})
+}
