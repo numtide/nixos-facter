f905f908a2fa0747aa7ecb4a0956e6ce24111d5a
Brian McGee <brian@bmcgee.ie>
2024-09-09 12:08:10 +0100

    doc: add godoc for build package.
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -1,10 +1,14 @@
+// Package build contains constants and values set at build time via -X flags.
 package build
 
 var (
-	Name    = "nixos-facter"
+	// Name is the program name, typically set via Nix to match the derivation's `pname`.
+	Name = "nixos-facter"
+	// Version is the program version, typically set via Nix to match the derivation's `version`.
 	Version = "v0.0.0+dev"
-	System  = ""
-
-	// ReportVersion is used to indicate significant changes in the report output
+	// System is the architecture that this program was built for e.g. x86_64-linux.
+	// It is set via Nix to match the Nixpkgs system.
+	System = ""
+	// ReportVersion is used to indicate significant changes in the report output and is embedded JSON report produced.
 	ReportVersion uint = 2
 )
